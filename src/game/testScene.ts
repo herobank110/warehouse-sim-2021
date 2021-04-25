2e076a21a2a140e63c82f895197655f06f7917cd
David Kanekanian <dkanekanian1@gmail.com>
2021-04-25 19:20:04 +0100

    start drag function

--- src/game/testScene.ts
+++ src/game/testScene.ts
@@ -2,68 +2,79 @@ import { Scene, Engine, Actor, Color, vec, Sprite } from 'excalibur';
 import { R } from '../utils';
 import { tileCoords, zero } from '../utils/vector';
 
+let dragFrom: Actor | undefined;
+function startDrag(from: Actor) {
+  dragFrom = from;
+}
+function endDrag(to: Actor) {
+  if (dragFrom == scenery[0]) {
+    console.log('hi');
+  }
+  dragFrom = undefined;
+}
+
+// box, box, triangle
+const items = [
+  new Actor({
+    currentDrawing: new Sprite(R.texture.square, 0, 0, 7, 7),
+    pos: tileCoords(vec(0, 0)).add(vec(2, 2)),
+    anchor: zero(),
+  }),
+  new Actor({
+    currentDrawing: new Sprite(R.texture.square, 0, 0, 7, 7),
+    pos: tileCoords(vec(0, 0)).add(vec(2, 10)),
+    anchor: zero(),
+  }),
+  new Actor({
+    currentDrawing: new Sprite(R.texture.triangle, 0, 0, 7, 7),
+    pos: tileCoords(vec(0, 0)).add(vec(2, 18)),
+    anchor: zero(),
+  }),
+];
+
+// s/r, shelf1, shelf2
+const scenery = [
+  new Actor({
+    pos: tileCoords(vec(0, 0)),
+    width: 14,
+    height: 28,
+    color: Color.fromHex('dbc751'),
+    anchor: zero(),
+    enableCapturePointer: true,
+  }),
+  // no depot for now
+  // new Actor({
+  //   pos: tileCoords(vec(0, 1)),
+  //   width: 14,
+  //   height: 28,
+  //   color: Color.fromHex('51b6db'),
+  //   anchor: zero(),
+  // }),
+  new Actor({
+    pos: tileCoords(vec(1, 0)),
+    width: 28,
+    height: 14,
+    color: Color.fromHex('afafaf'),
+    anchor: zero(),
+  }),
+  new Actor({
+    pos: tileCoords(vec(1, 1)),
+    width: 28,
+    height: 14,
+    color: Color.fromHex('afafaf'),
+    anchor: zero(),
+  }),
+];
+
 export default (game: Engine) => {
   // game.input.pointers.primary.on('move', e => {});
   // game.input.pointers.primary.on('down', e => {});
 
   const scene = new Scene(game);
 
-  const items = [
-    new Actor({
-      currentDrawing: new Sprite(R.texture.square, 0, 0, 7, 7),
-      pos: tileCoords(vec(0, 0)).add(vec(2, 2)),
-      anchor: zero(),
-    }),
-    new Actor({
-      currentDrawing: new Sprite(R.texture.square, 0, 0, 7, 7),
-      pos: tileCoords(vec(0, 0)).add(vec(2, 10)),
-      anchor: zero(),
-    }),
-    new Actor({
-      currentDrawing: new Sprite(R.texture.triangle, 0, 0, 7, 7),
-      pos: tileCoords(vec(0, 0)).add(vec(2, 18)),
-      anchor: zero(),
-    }),
-  ];
-
-  const scenery = [
-    new Actor({
-      pos: tileCoords(vec(0, 0)),
-      width: 14,
-      height: 28,
-      color: Color.fromHex('dbc751'),
-      anchor: zero(),
-      enableCapturePointer: true,
-    }),
-    // no depot for now
-    // new Actor({
-    //   pos: tileCoords(vec(0, 1)),
-    //   width: 14,
-    //   height: 28,
-    //   color: Color.fromHex('51b6db'),
-    //   anchor: zero(),
-    // }),
-    new Actor({
-      pos: tileCoords(vec(1, 0)),
-      width: 28,
-      height: 14,
-      color: Color.fromHex('afafaf'),
-      anchor: zero(),
-    }),
-    new Actor({
-      pos: tileCoords(vec(1, 1)),
-      width: 28,
-      height: 14,
-      color: Color.fromHex('afafaf'),
-      anchor: zero(),
-    }),
-  ];
-
   [...scenery, ...items].map(i => scene.add(i));
 
-  scenery[0].on('pointerdown', () => {
-    console.log('pointerdown');
-  });
+  scenery[0].on('pointerdown', e => console.log(e));
 
   scene.camera.pos.setTo(100, 100);
   scene.camera.zoom(2);
