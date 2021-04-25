eb8ebdd452471aef2b8c407d2b1949a658e19483
David Kanekanian <dkanekanian1@gmail.com>
2021-04-25 10:48:45 +0100

    add more sample boxes for drawing initial compound

--- src/game/testScene.ts
+++ src/game/testScene.ts
@@ -1,16 +1,52 @@
-import { Scene, Engine, Actor, Color } from 'excalibur';
+import { Scene, Engine, Actor, Color, Label, vec, TextAlign } from 'excalibur';
+
+const Zero = () => vec(0, 0);
+
 export default (game: Engine) => {
   const scene = new Scene(game);
-  const srBay = new Actor({
-    x: 80,
-    y: 80,
-    width: 28,
-    height: 28,
-    color: Color.fromHex('dbc751'),
-  });
+  [
+    new Actor({
+      x: 80,
+      y: 80,
+      width: 14,
+      height: 28,
+      color: Color.fromHex('dbc751'),
+      anchor: Zero()
+    }),
+    // new Label({ text: 'S&R', x: 80, y: 80, textAlign: TextAlign.Center }),
+    // new Label({ text: 'bay', x: 80, y: 90, textAlign: TextAlign.Center }),
+    new Actor({
+      x: 80,
+      y: 109,
+      width: 14,
+      height: 28,
+      color: Color.fromHex('51b6db'),
+      anchor: Zero()
+    }),
+    // new Label({ text: 'de', x: 80, y: 109, textAlign: TextAlign.Center }),
+    // new Label({ text: 'pot', x: 80, y: 119, textAlign: TextAlign.Center }),
+    new Actor({
+      x: 109,
+      y: 80,
+      width: 28,
+      height: 14,
+      color: Color.fromHex('afafaf'),
+      anchor: Zero()
+    }),
+    new Actor({
+      x: 109,
+      y: 109,
+      width: 28,
+      height: 14,
+      color: Color.fromHex('afafaf'),
+      anchor: Zero()
+    }),
+    // new Label({ text: 'de', x: 80, y: 109, textAlign: TextAlign.Center }),
+    // new Label({ text: 'pot', x: 80, y: 119, textAlign: TextAlign.Center }),
+  ].map(s => scene.add(s));
 
-  scene.add(srBay);
   scene.camera.pos.setTo(100, 100);
   scene.camera.zoom(2);
+
   return scene;
 };
