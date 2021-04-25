65b98cce9ec41a25643055267f3865fbc8ff54ac
David Kanekanian <dkanekanian1@gmail.com>
2021-04-25 18:53:40 +0100

    new item textures

--- src/utils/r.ts
+++ src/utils/r.ts
@@ -1,10 +1,14 @@
 import { Texture } from 'excalibur';
 import truckUrl from 'url:../../res/truck.png';
 import srBayUrl from 'url:../../res/srBay.png';
+import squareUrl from 'url:../../res/square.png';
+import triangleUrl from 'url:../../res/triangle.png';
 
 export const R = {
   texture: {
     truck: new Texture(truckUrl),
     srBay: new Texture(srBayUrl),
+    square: new Texture(squareUrl),
+    triangle: new Texture(triangleUrl),
   } as const,
 } as const;
