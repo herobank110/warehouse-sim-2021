b9b8f48f2c6838db5d97bbc379976aca7e6cf1f3
David Kanekanian <dkanekanian1@gmail.com>
2021-04-26 22:43:58 +0100

    add item anchor zero

--- src/actors/item.ts
+++ src/actors/item.ts
@@ -1,7 +1,12 @@
 import { Actor, Engine, Sprite, Vector } from 'excalibur';
 import { R } from '../utils';
 
-export class BasicItem extends Actor {}
+export class BasicItem extends Actor {
+  onInitialize(engine: Engine) {
+    super.onInitialize(engine);
+    this.anchor.setTo(0, 0);
+  }
+}
 
 export class Square extends BasicItem {
   private static readonly sprite = new Sprite(R.texture.square, 0, 0, 7, 7);
