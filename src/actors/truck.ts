1d2540da05367441e42a88d6a366113ffac1382e
David Kanekanian <dkanekanian1@gmail.com>
2021-04-27 20:22:32 +0100

    add truck class with state

--- src/actors/truck.ts
+++ src/actors/truck.ts
@@ -1,9 +1,28 @@
-import { Actor, Engine } from 'excalibur';
+import { Actor, Engine, Sprite } from 'excalibur';
+import { R } from '../utils';
+import { BasicItem } from './item';
+
+const truckSprite = new Sprite(R.texture.truck, 0, 0, 59, 15);
+
+export class PickUp {
+  have: BasicItem[] = [];
+  need: BasicItem[] = [];
+}
+
+export class DropOff {
+  items: BasicItem[] = [];
+}
+
+export type TruckState = PickUp | DropOff;
 
 export class Truck extends Actor {
+  truckState: TruckState = new PickUp();
+
   onInitialize(engine: Engine) {
     super.onInitialize(engine);
 
-    this.
+    this.addDrawing(truckSprite);
   }
+
+  enterLoadingBay() {}
 }
