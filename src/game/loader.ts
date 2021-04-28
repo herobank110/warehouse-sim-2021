50c67ad99a5d5df6ae23d11f3bc9c2cab4337072
David Kanekanian <dkanekanian1@gmail.com>
2021-04-28 19:39:04 +0100

    add new custom logo

--- src/game/loader.ts
+++ src/game/loader.ts
@@ -1,10 +1,13 @@
 import { Loader } from 'excalibur';
 import { R } from '../utils';
+// @ts-ignore
+import logoUrl from 'url:../../res/logo.png';
 
 export default class extends Loader {
   constructor() {
     super(Object.values(R.texture));
     this.playButtonText = 'Start';
-    // this.logo = ""
+    this.logo = logoUrl;
+    this.backgroundColor = R.color.bg.toHex();
   }
 }
