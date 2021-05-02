ad9b4caefd9cd3d4f7ea18e18af7404af75ead09
David Kanekanian <dkanekanian1@gmail.com>
2021-05-02 23:01:51 +0100

    remove 'score' label unnecessary confusing

--- src/ui/gameOverScreen.ts
+++ src/ui/gameOverScreen.ts
@@ -17,7 +17,7 @@ const makeGameOverScreenUi = (data: { score: number; onDone: () => void }) =>
     .append(
       $('<h2>', { text: 'Game Over' }),
       $('<div>').append(
-        $('<span>', { text: 'Score:' }),
+        // $('<span>', { text: 'Score:' }),
         $('<span>', { text: data.score }),
         $('<div>', { css: { backgroundImage: `url('${R.texture.suitcase.path}')` } }),
         $('<span>', { text: 'items delivered' }),
